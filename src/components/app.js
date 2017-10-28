76a340fb42fe2f8ac41a295b94e7cea32f45b1d8
Ephraim Glick <ephraim.glick@gmail.com>
2017-10-28 11:45:37 -0700

    Initialize with live data

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,33 @@
+var fakeVideo = {
+  id: { videoId: "" },
+  snippet: { title: "", description: "", thumbnails: 
+    { default: { url: "", width: 120, height: 90 } }
+  }
+};
+
+var fakeVideos = Array(5).fill(fakeVideo);
+
+// console.dir(fakeVideos);
+
 angular.module('video-player')
   .component('app', {
-    controller: function() {
-      this.videos = exampleVideoData;
-      this.currentVideo = exampleVideoData[0];
+    controller: function(youTube) {
+      this.videos = fakeVideos;
+      this.currentVideo = fakeVideo;
       this.searchResults = (results) => {
+        console.log(results.data.items);
         this.videos = results.data.items;
         this.currentVideo = results.data.items[0];
       };
       this.selectVideo = (video) => {
         this.currentVideo = video;
       };
+      this.$onInit = () => {
+        console.log('App initialized!');
+        youTube.search('hawaii', this.searchResults.bind(this));
+      };
     },
     templateUrl: 'src/templates/app.html'
   });
+
+
